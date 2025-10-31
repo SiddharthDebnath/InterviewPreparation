d9be2eeb642293c972ba6ccdf8f7ceaf1db8d422
Sid <sid6teen@gmail.com>
2025-10-30 22:11:41 -0500

    Added new ways to solve the same coding problem

--- Arrays_and_Hashing/Duplicates.py
+++ Arrays_and_Hashing/Duplicates.py
@@ -1,12 +1,35 @@
-class Solution:
-    def containsDuplicate(self, nums):
-        container = {}
-        for i in nums:
-            if i in container:
+
+def bruteContainsDuplicate(nums):
+    for i in range(len(nums)-1):
+        for j in range(i+1,len(nums)):
+            if nums[i] == nums[j]:
                 return True
-            else:
-                container[i] = 1
+    return False
+
+
+def hashMapContainsDuplicate(nums):
+    container = {}
+    for i in nums:
+        if i in container:
+            return True
+        else:
+            container[i] = 1
+    return False
+
+def sortedArrayContainsDuplicate(nums):
+    nums.sort()
+    for i in range(len(nums)-1):
+        if nums[i] == nums[i+1]:
+            return True
+    return False
+
+def setContainsDuplicate(nums):
+    if len(nums) == len(set(nums)):
         return False
+    else:
+        return True
 
-
-print(Solution.containsDuplicate([1, 2, 3, 1]))
+print(bruteContainsDuplicate([1, 2, 3, 1]))
+print(hashMapContainsDuplicate([1, 2, 3]))
+print(sortedArrayContainsDuplicate([1, 2, 3]))
+print(setContainsDuplicate([1, 2, 3, 6]))
--- Arrays_and_Hashing/isAnagram.py
+++ Arrays_and_Hashing/isAnagram.py
@@ -1,12 +1,52 @@
-def isAnagram(s, t):
+from collections import Counter
+
+
+def bruteIsAnagram(s, t):
+    s = s.lower()
+    t = t.lower()
     if len(s) != len(t):
         return False
+    CountS = {}
+    CountT = {}
+    if len(s) != len(t):
+        return False
+    for i in s:
+        if i in CountS:
+            CountS[i] += 1
+        else:
+            CountS[i] = 1
 
-    elif sorted(s.lower()) == sorted(t.lower()):
+    for i in t:
+        if i in CountT:
+            CountT[i] += 1
+        else:
+            CountT[i] = 1
+    print(CountS)
+    print(CountT)
+    if CountS == CountT:
+        return True
+    else:
+        return False
+
+
+
+
+def sortedIsAnagram(s, t):
+    if sorted(s.lower()) == sorted(t.lower()):
         return True
 
     else:
         return False
 
+def counterIsAnagram(s, t):
+    s = s.lower()
+    t = t.lower()
+    if Counter(s) == Counter(t):
+        return True
+    else:
+        return False
 
-print(isAnagram('Ship', 'Hips'))
+
+print(bruteIsAnagram('Ship', 'Hips'))
+print(sortedIsAnagram('Ship', 'Hips'))
+print(counterIsAnagram('Ship', 'Hips'))
